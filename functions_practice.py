e6b4ad4af30b9f6c1621970a1d09684a327ad3ad
nawright16 <nawright16@gmail.com>
2023-06-05 11:49:42 -0400

    function practice

--- functions_practice.py
+++ functions_practice.py
@@ -1,25 +1,61 @@
-def hello():
-    print("Hi there")
+def max_num(a, b, c):
+    return max([a, b, c])
+print(max_num(22, 13, 68))
 
-def pack(item1, item2, item3):
+
+def mult_list(lst):
+    if len(lst) == 0:
+        return 0
+    prod = lst[0]
+
+    if len(lst) > 1:
+        for i in lst[1:]:
+            prod = prod * i
+
+    return prod
+print(mult_list([2,7,4]))            
+
+
+def rev_string(str):
+    return str[::-1]
+
+print(rev_string("chocolate"))
+
+def num_within(x, y, z):
+    return x in range(y, z + 1)
+print(num_within(10, 9, 12))
+
+triangle = [[1],[1,1]]
+def pascal(n):
+  
+  if n < 1:
+    print("wrong number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
     
-   return [ item1, item2, item3]
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      
+      length = len(row_prev)+1
+      for i in range(length):
+        
+        if i == 0:
+          row.append(1)
+        
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
 
-
-def eat_lunch(my_list):
     
-    if len(my_list) == 0:
-        print("My lunchbox is empty!")
-
-    else:
-        for i in range(len(my_list)):
-            if i == 0:
-                print(f"First I eat {my_list[0]}")
-            else:
-                print(f"Next I eat {my_list[i]}") 
+    for row in triangle:
+      print(row)
 
 
-
-
-
-
+pascal(8)
